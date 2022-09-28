85c63b5b25d2bfc79da60a56b6f8ebb1e041051b
Lucy Quayson <quaysonlucyofficial@gmail.com>
2022-09-28 19:46:46 +0000

    Contact Section

--- responsive-portfolio-website-chris-main/assets/js/main.js
+++ responsive-portfolio-website-chris-main/assets/js/main.js
@@ -65,7 +65,47 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 });
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactProject = document.getElementById('contact-project'),
+    contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // Check if the field has a value
+    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+        // Add and remove color
+        contactMessage.classList.remove('color-blue')
+        contactMessage.classList.add('color-red')
+
+        // Show message
+        contactMessage.textContent = 'Write all the input fields 📩'
+    } else {
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_gute2ll', 'template_kjsvh06', '#contact-form', 'oCNvTs6M2s3ee57VI')
+            .then(() => {
+                // Show message and add color
+                contactMessage.classList.add('color-blue')
+                contactMessage.textContent = 'Message sent ✅'
+
+                // Remove message after five seconds
+                setTimeout(() => {
+                    contactMessage.textContent = ''
+                }, 5000)
+            }, (error) => {
+                alert('OOPS! SOMETHING HAS FAILED...', error)
+            })
+
+        // To clear the input field
+
+        contactName.value = ''
+        contactEmail.value = ''
+        contactProject.value = ''
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
